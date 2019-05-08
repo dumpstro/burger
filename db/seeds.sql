a1618ba21b74a0d805de9e5e2e9b9da93bece35a
Justin Summers <jsummers07@gmail.com>
2019-05-07 21:44:58 -0400

    initial file push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mahi Mahi Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Kahuana Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("In-N-Out Burger" , false);
+
