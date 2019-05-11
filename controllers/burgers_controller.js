ce415ae291a263e8d7ae1792bd4de086204ed285
Justin Summers <jsummers07@gmail.com>
2019-05-10 21:39:00 -0400

    frontend completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,14 +16,14 @@ router.post("/api/burgers", function(req, res) {
     burger.insertOne([
         "burger_name", "devoured" 
     ], [
-        req.body.burger_name, false
+        req.body.burger_name, req.body.devoured
     ], function(result) {
         //Send back the ID of the new burger
         res.json({ id: result.insertID });
     });
 });
 
-router.put("/api/cats/:id", function(req,res) {
+router.put("/api/burgers/:id", function(req,res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
